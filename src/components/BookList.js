ee018dcbcee1b9f58cacc023626a266620a7d96a
jbvikram@outlokk.com <jbvikram@outlook.com>
2024-01-20 19:59:01 +0530

    first commit

---
+++ src/components/BookList.js
@@ -0,0 +1,18 @@
+import BookShow from "./BookShow";
+import useBookContext from "../hooks/use-books";
+
+
+function BookList() {
+    const { books } = useBookContext();
+   
+
+    const renderedBooks = books.map((book) => {
+       return <BookShow  key={book.id} book={book} />
+    });
+    return<div className="book-list">
+        
+        {renderedBooks}
+        </div>;
+}
+
+export default BookList;
---
+++ src/hooks/use-books.js
@@ -0,0 +1,9 @@
+import { useContext } from "react";
+import BookContext from "../context/book";
+
+function useBookContext () {
+    return useContext(BookContext);
+}
+
+
+export default useBookContext;
